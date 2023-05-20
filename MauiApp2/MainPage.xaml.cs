8d14d67440863cbf8499d07d6c89a2709e67abad
Piotr Romanowski <piotr@romanowski.me>
2023-05-20 13:15:05 +0200

    Initial commit

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        DotnetBotImage.Source = "dotnet_bot.png";
+    }
+}
